cf22ea72ab5b745fb99f8b23daf5e8c632a9f8d4
annashebanova <2shebanova@gmail.com>
2021-10-19 17:47:36 +0300

    .

---
+++ js/main.js
@@ -0,0 +1,18 @@
+function topBarHide() {
+    let prevScrollPos = window.pageYOffset;
+    window.addEventListener(
+        "scroll", () => {
+            let currentScollPos = window.pageYOffset;
+            const topBar = document.querySelector(".topbar");
+            if (prevScrollPos > currentScollPos || prevScrollPos <= 0) {
+                topBar.removeAttribute("style");
+             }
+            else {
+                topBar.style.transform = "translateY(-100%)";
+            }
+            
+            prevScrollPos = currentScollPos;
+  }  
+ )
+};
+topBarHide();
